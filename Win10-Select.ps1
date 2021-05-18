12b01f6fb3c7c75d8355113e05711554abc95f40
ChrisStro <christopher.strobel@outlook.de>
2021-05-18 18:08:39 +0200

    add support for Provisioning Packages
    needed for zti via autopilot over wifi

--- Win10-Select.ps1
+++ Win10-Select.ps1
@@ -19,13 +19,24 @@ Write-Host  -ForegroundColor Cyan "Ejecting ISO"
 Write-Warning "That didn't work because I haven't coded it yet!"
 #Start-Sleep -Seconds 5
 
+#Using My Custom Script
+$dotScript = "WinREProvPackage.ps1"
+if (Test-Path $dotScript) {
+    Write-Host  -ForegroundColor Magenta "Dot-Sourcing functions from $dotScript "
+    . $dotScript
+    $ProvPackages = Select-OSDCloudProvPackage
+}
+
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
 Start-OSDCloud -OSLanguage de-de
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
-Write-Warning "I'm not sure of what to put here yet"
+
+if ($ProvPackages) {
+    $ProvPackages | Add-OSDCloudProvPackage
+}
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
