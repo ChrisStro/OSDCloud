c8eeb0397cc8e6e0f63177757c0227f663e51d61
ChrisStro <christopher.strobel@outlook.de>
2021-07-01 15:48:35 +0200

    Update Win10_ZTI-OSDProgess.ps1

--- Win10_ZTI-OSDProgess.ps1
+++ Win10_ZTI-OSDProgess.ps1
@@ -2,18 +2,24 @@ Watch-OSDCloudProvisioning {
     Write-Host -ForegroundColor Cyan "Hey this script running an OSD Cloud ZTI Deployment while displaying a MahApps.Metro progress Window"
 
     #Start OSDCloud ZTI
-    Write-Host  -ForegroundColor Cyan "Starting OSDCloud PreAction stuff..."
+    Update-OSDProgress -Text "Starting OSDCloud PreAction stuff..." # output to UI
+    Write-Host  -ForegroundColor Cyan "Starting OSDCloud PreAction stuff..." # output to console
     Start-Sleep -Seconds 5
+    Update-OSDProgress -Text " " # hide first text
 
     Start-OSDCloud -OSBuild 20H2 -OSEdition Pro -ZTI
 
     #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
+    Update-OSDProgress -Text "Starting OSDCloud PostAction stuff..."
     Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction stuff..."
+    Start-Sleep -Seconds 5
+    Update-OSDProgress -Text " " # hide first text
 
     # lets throw an error, just for fun
-    Update-OSDProgress -DisplayError "Custom error message, pls unlock screen!"
+    #Update-OSDProgress -DisplayError "Custom error message, pls unlock screen!"
 
     #Restart from WinPE
-    #Start-Sleep -Seconds 20
-    #wpeutil reboot
-} -Window
+    Update-OSDProgress -Text "Reboot in 20 seconds"
+    Start-Sleep -Seconds 20
+    wpeutil reboot
+}
