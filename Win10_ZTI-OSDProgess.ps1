e2f09337131fcfe175238494bbd688630f2a586e
ChrisStro <christopher.strobel@outlook.de>
2021-07-05 10:52:26 +0200

    Update Win10_ZTI-OSDProgess.ps1

--- Win10_ZTI-OSDProgess.ps1
+++ Win10_ZTI-OSDProgess.ps1
@@ -1,22 +1,23 @@
 Watch-OSDCloudProvisioning {
     Write-Host -ForegroundColor Cyan "Hey this script running an OSD Cloud ZTI Deployment while displaying a MahApps.Metro progress Window"
 
-    #Start OSDCloud ZTI, Update Module first
+    # Update Module first
     Update-OSDProgress -Text "Starting OSDCloud PreAction stuff..." # output to UI
     Write-Host  -ForegroundColor Cyan "Starting OSDCloud PreAction stuff..." # output to console    
     Start-Sleep -Seconds 3
     Update-OSDProgress -Text "Updating and Import the awesome OSD PowerShell Module"
     Install-Module OSD -Force
     Import-Module OSD -Force
-    Update-OSDProgress -Text " " # hide first text
-
+    
+    # Start OSD Cloud and let the user know
+    Update-OSDProgress -Text "Start OSDCloud : OSBuild 20H2"
     Start-OSDCloud -OSBuild 20H2 -OSEdition Pro -ZTI
 
-    #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
+    # Doning Postaction stuff
     Update-OSDProgress -Text "Starting OSDCloud PostAction stuff..."
     Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction stuff..."
     Start-Sleep -Seconds 5
-    Update-OSDProgress -Text " " # hide first text
+    Update-OSDProgress -Text " " # hide current text
 
     # lets throw an error, just for fun
     #Update-OSDProgress -DisplayError "Custom error message, pls unlock screen!"
