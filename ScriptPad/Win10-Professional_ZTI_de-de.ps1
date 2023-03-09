d89ca95b1314ecc64c40f82724ec49fc78375fe6
ChrisStro <christopher.strobel@outlook.de>
2023-03-09 22:34:01 +0100

    Update and rename Win10_ZTI-Professional-DE-DE.ps1 to ScriptPad/Win10-Professional_ZTI_de-de.ps1

---
+++ ScriptPad/Win10-Professional_ZTI_de-de.ps1
@@ -0,0 +1,21 @@
+Write-Host  -ForegroundColor Cyan "Starting SeguraOSD's Custom OSDCloud ..."
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Start OSDCloud ZTI the RIGHT way
+Write-Host -ForegroundColor Green "Start OSDCloud in cloud mode"
+Start-OSDCloud -OSBuild 22H2 -OSEdition Pro -OSLanguage de-de -ZTI
+
+#Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
+Write-Warning "I'm not sure of what to put here yet"
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
